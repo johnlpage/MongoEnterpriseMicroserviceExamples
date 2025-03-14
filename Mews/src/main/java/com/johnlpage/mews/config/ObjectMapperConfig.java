c36bc3281106516bc7db92f48c75d5d97f23630c
John Page <john.page@10gen.com>
2025-03-14 10:00:09 +0000

    Added ObjectId custom Deserializer code.

--- Mews/src/main/java/com/johnlpage/mews/config/ObjectMapperConfig.java
+++ Mews/src/main/java/com/johnlpage/mews/config/ObjectMapperConfig.java
@@ -3,7 +3,9 @@ package com.johnlpage.mews.config;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.bson.types.ObjectId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.buffer.DataBufferFactory;
@@ -14,9 +16,14 @@ public class ObjectMapperConfig {
 
   @Bean
   public ObjectMapper objectMapper(JsonFactory jsonFactory) {
-    return new ObjectMapper(jsonFactory)
-        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-        .registerModule(new JavaTimeModule());
+    ObjectMapper mapper =
+        new ObjectMapper(jsonFactory)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .registerModule(new JavaTimeModule());
+    SimpleModule module = new SimpleModule();
+    module.addSerializer(ObjectId.class, new ObjectIdSerializer());
+    mapper.registerModule(module);
+    return mapper;
   }
 
   @Bean
